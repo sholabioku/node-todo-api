e805556f854c64e1892ac80a50eae19af79bc2f9
Shola Bioku <bilushonline@gmail.com>
2020-10-14 10:21:46 +0100

    Add POST /todos route and refracture mongoose

---
+++ server/routes/todos.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+
+const { Todo } = require('../models/todo');
+
+router.post('/', (req, res) => {
+  const todo = new Todo({
+    text: req.body.text,
+  });
+  todo
+    .save()
+    .then((doc) => res.send(doc))
+    .catch((err) => res.status(400).send(err));
+});
+
+module.exports = router;
---
+++ server/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const { mongoose } = require('./db/mongoose');
+const todos = require('./routes/todos');
+const users = require('./routes/users');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use('/todos', todos);
+
+app.listen(3000, () => console.log('Started on port 3000'));
