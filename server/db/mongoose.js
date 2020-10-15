22af21fa6a452702483861989de1e658188dec34
Shola Bioku <bilushonline@gmail.com>
2020-10-15 11:53:43 +0100

    Install lodash and Add PATCH /todos/:id route

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -6,6 +6,7 @@ mongoose.connect(
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
+    useFindAndModify: false,
   }
 );
 
--- server/routes/todos.js
+++ server/routes/todos.js
@@ -1,3 +1,4 @@
+const _ = require('lodash');
 const express = require('express');
 const router = express.Router();
 
@@ -52,6 +53,31 @@ router.delete('/:id', (req, res) => {
         return res.status(404).send();
       }
 
+      res.send({ todo });
+    })
+    .catch((err) => res.status(400).send(err));
+});
+
+router.patch('/:id', (req, res) => {
+  const id = req.params.id;
+  const body = _.pick(req.body, ['text', 'completed']);
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  if (_.isBoolean(body.completed) && body.completed) {
+    body.completedAt = new Date().getTime();
+  } else {
+    body.completed = false;
+    body.completedAt = null;
+  }
+
+  Todo.findByIdAndUpdate(id, { $set: body }, { new: true })
+    .then((todo) => {
+      if (!todo) {
+        return res.status(404).send();
+      }
       res.send({ todo });
     })
     .catch((err) => res.status(400).send(err));
