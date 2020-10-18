16266323aa772d81e0bcd44916c16a5a8b7fafb1
Shola Bioku <bilushonline@gmail.com>
2020-10-18 09:54:29 +0100

    Hash user password with pre-save method

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,5 +1,6 @@
 const crypto = require('crypto');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
 const message = 'I am message 3';
 
@@ -10,7 +11,22 @@ const hash = crypto.createHash('sha256').update(message).digest('hex');
 const data = { id: 10 };
 
 const token = jwt.sign(data, '123abc');
-console.log(token);
+// console.log(token);
 
 const decoded = jwt.verify(token, '123abc');
-console.log('decoded: ', decoded);
+// console.log('decoded: ', decoded);
+
+const password = '123abc!';
+
+bcrypt.genSalt(10, (err, salt) => {
+  bcrypt.hash(password, salt, (err, res) => {
+    // console.log(res);
+  });
+});
+
+const hashedPassword =
+  '$2a$10$PflJzK5eXXJbQuYsmZ0peOvNuZ13siazEuPDZqc7xHApAPOpbMYLy';
+
+bcrypt.compare(password, hashedPassword, (err, res) => {
+  console.log(res);
+});
