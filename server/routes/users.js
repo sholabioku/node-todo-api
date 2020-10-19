54bebc7e6fdb96787db3c91f42407f48e997ddcd
Shola Bioku <bilushonline@gmail.com>
2020-10-19 09:33:14 +0100

    Add POST /users/login route

--- server/routes/users.js
+++ server/routes/users.js
@@ -20,6 +20,20 @@ router.post('/register', (req, res) => {
     .catch((err) => res.status(400).send(err));
 });
 
+router.post('/login', (req, res) => {
+  const body = _.pick(req.body, ['email', 'password']);
+
+  User.findByCredentials(body.email, body.password)
+    .then((user) => {
+      return user.generateAuthToken().then((token) => {
+        res.header('x-auth', token).send(user);
+      });
+    })
+    .catch((err) => {
+      res.status(400).send(err);
+    });
+});
+
 router.get('/me', authenticate, (req, res) => {
   res.send(req.user);
 });
