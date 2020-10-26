3f1861098167f40a5db3f02c331d800866f3dc2b
Shola Bioku <bilushonline@gmail.com>
2020-10-26 17:15:43 +0100

    Install express-mongo-sanitize and prevent nosql injection

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ require('./config/config');
 const express = require('express');
 const colors = require('colors');
 const bodyParser = require('body-parser');
+const mongoSanitize = require('express-mongo-sanitize');
 
 const { mongoose } = require('./db/mongoose');
 const todos = require('./routes/todos');
@@ -11,6 +12,7 @@ const users = require('./routes/users');
 const app = express();
 
 app.use(bodyParser.json());
+app.use(mongoSanitize());
 
 app.use('/todos', todos);
 app.use('/users', users);
