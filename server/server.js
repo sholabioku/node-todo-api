e676727ae1556d51585a65dd03b159b667a4ba77
sholabioku <bilushonline@gmail.com>
2021-06-24 14:06:05 +0100

    A fix for heroku error

--- server/server.js
+++ server/server.js
@@ -1,21 +1,21 @@
-require('./config/config');
+require("./config/config");
 
-const express = require('express');
-const colors = require('colors');
-const morgan = require('morgan');
-const helmet = require('helmet');
-const xss = require('xss-clean');
-const bodyParser = require('body-parser');
-const mongoSanitize = require('express-mongo-sanitize');
+const express = require("express");
+const colors = require("colors");
+const morgan = require("morgan");
+const helmet = require("helmet");
+const xss = require("xss-clean");
+const bodyParser = require("body-parser");
+const mongoSanitize = require("express-mongo-sanitize");
 
-const { mongoose } = require('./db/mongoose');
-const todos = require('./routes/todos');
-const users = require('./routes/users');
+const { mongoose } = require("./db/mongoose");
+const todos = require("./routes/todos");
+const users = require("./routes/users");
 
 const app = express();
 
-if (process.env.NODE_ENV === 'development') {
-  app.use(morgan('dev'));
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
 }
 
 app.use(bodyParser.json());
@@ -23,8 +23,8 @@ app.use(mongoSanitize());
 app.use(helmet());
 app.use(xss());
 
-app.use('/todos', todos);
-app.use('/users', users);
+app.use("/todos", todos);
+app.use("/users", users);
 
 const port = process.env.PORT;
 app.listen(port, () =>
