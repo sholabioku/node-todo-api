8070292b28525717c4525dcc9745ecb180d662f0
sholabioku <bilushonline@gmail.com>
2021-06-29 12:08:56 +0100

    Add fix for heroku

--- server/config/config.js
+++ server/config/config.js
@@ -1,6 +1,6 @@
 const env = process.env.NODE_ENV || "development";
 
-if (env === "development" || env === "test" || env === "production") {
+if (env === "development" || env === "test") {
   const config = require("./config.json");
   const envConfig = config[env];
   Object.keys(envConfig).forEach((key) => {
